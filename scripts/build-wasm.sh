a67e4279c7ed0b30aa467d8cec305ccfc2951730
Antonio Malara <antonio.malara@gmail.com>
2022-01-21 19:07:52 +0100

    Update build for new raylib

--- scripts/build-wasm.sh
+++ scripts/build-wasm.sh
@@ -1,6 +1,10 @@
 SRC_DIR="../src"
 DATA_DIR="../data"
 
+RAY_DIR="../vendor/raylib-4.0.0_webassembly/"
+RAY_INCLUDE="${RAY_DIR}/include"
+RAY_LIB="${RAY_DIR}/lib"
+
 SOURCES="${SRC_DIR}/Decoders.cpp ${SRC_DIR}/Models.cpp ${SRC_DIR}/Resources.cpp ${SRC_DIR}/main.cpp"
 
-emcc --std=gnu++1z -o index.html ${SOURCES} -s USE_GLFW=3 --preload-file "${DATA_DIR}@/data/" -L../vendor/raylib-3.7.0_wasm/lib/ -lraylib -s ASYNCIFY
+emcc --std=gnu++1z -o index.html -I"${RAY_INCLUDE}" ${SOURCES} -s USE_GLFW=3 --preload-file "${DATA_DIR}@/data/" -L"${RAY_LIB}" -lraylib -s ASYNCIFY
